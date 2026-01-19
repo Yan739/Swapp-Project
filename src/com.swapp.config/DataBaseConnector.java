616b06f140f071b3b1324ef234a8f9ea4d4e7170
Yann Cabrel NGATEU TAGNE <ngateuyann8@gmail.com>
2026-01-19 06:39:31 +0100

    feat: Database link with project

--- src/com.swapp.config/DataBaseConnector.java
+++ src/com.swapp.config/DataBaseConnector.java
@@ -1,21 +1,27 @@
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DataBaseConnector {
     private static Connection connect;
 
     private String url = ${JDBC_URL}
-    private String name = ${DB_USER};
+    private String user = ${DB_USER};
     private String password = ${DB_PASSWORD};
 
     private DataBaseConnector(){
         try {
-            connect = DriverManager.getConnection();
+
+            connect = DriverManager.getConnection(url, user, password);
+
+        }catch (SQLException e){
+            System.out.printf("Erreur de connection ! \nNous avons rencontrer cette érreur : %s", e);
         }
     }
 
     public static Connection getInstance(){
         if (connect == null){
-
+            new DataBaseConnector();
         }
     }
 }
